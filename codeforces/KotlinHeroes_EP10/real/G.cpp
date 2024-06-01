ccfb216e641357d44009f621aeb8c838a92ed797
Awei <Awei0420@gmail.com>
2024-06-01 16:19:26 +0800

    Add 2928. Distribute Candies Among Children I to master

--- codeforces/KotlinHeroes_EP10/real/G.cpp
+++ codeforces/KotlinHeroes_EP10/real/G.cpp
@@ -4,9 +4,22 @@
 using namespace std;
 typedef unsigned long long ULL;
 typedef long long LL;
+int res = 0;
 
-void solve() {
-    
+void dfs(int n, int limit, int cnt) {
+    if (cnt == 3) {
+        if (!n) ++res;
+        return;
+    }
+    for (int i = 0; i <= limit; ++i) {
+        dfs(n-i, limit, cnt+1);
+    }
+}
+
+void solve() { //2928. Distribute Candies Among Children I
+    if (limit*3 < n) return 0;
+    dfs(n, limit, 0);
+    cout << res << endl;
 }
 
 int main() {
